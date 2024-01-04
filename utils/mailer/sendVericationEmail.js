ee361be5bf041c1d2911560a85fa9eeee217bb4a
datboigbenga <95724380+datboigbenga@users.noreply.github.com>
2024-01-04 17:09:39 +0100

    Update sendVericationEmail.js

--- utils/mailer/sendVericationEmail.js
+++ utils/mailer/sendVericationEmail.js
@@ -1,10 +1,9 @@
 const sendEmail =require("./sendEmail");
 
 
-const sendVerificationEmail= async({userName, email, verificationPin})=>{
+const sendVerificationEmail= async({userName, email, verificationToken})=>{
 
-    const message = `<h5>please input the 6 digit pin attached to this mail to verify your email
-    Here-> ${verificationPin}`
+    const message = `<h5>please input the 5 digit pin attached to this mail to verify your email <br>${verificationToken}</h5>`
     return sendEmail({
         to:email,
         subject: "Email Confirmation",
